3764912b6fd01cd3eefe4ebb16bcdcbd55e7a241
anandhu suresh <sureshanandhu50@gmail.com>
2021-11-23 10:34:32 +0530

    second commit

---
+++ forexmple.py
@@ -0,0 +1,9 @@
+fruits = ["apple","banana","cherry","orange","pineapple","watermelon","Grapes"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+for y in fruits:
+  print(y)
+  if y =="watermelon":
+      break
